2a879561ef516ef926335faa7ef04ccdda54a207
Saurav Barua <myallmailofficial@gmail.com>
2021-01-25 16:31:33 +0100

    update but its not work

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import './App.css';
 
 
-var constantes = [
-  {tempMin:-20},
-  {tempMax:40},
-  {heartMin:80},
-  {heartMax:180},
-  {stepsMin:0},
-  {stepsMax:50000}
-];
+var constantes = {
+"tempMin":-20,
+"tempMax":40,
+"heartMin": 80,
+"heartMax":180,
+"stepsMin":0,
+"stepsMax":50000
+
+
+};
+  
 
 export class App extends React.Component {
   render() {
@@ -18,9 +21,9 @@ export class App extends React.Component {
         <h1 className="text-primary ">Hello !</h1>
         <div className="container-fluid">
         <div className="row">
-          <P>Heart :{this.constantes[2]} </P>
-          <P>Temperature :{this.constantes[0]}</P>
-          <P>Steps :{this.constantes[4]}</P>
+          <P>Heart :{constantes.heartMin} </P>
+          <P>Temperature :{constantes.tempMin}</P>
+          <P>Steps :{constantes.stepsMin}</P>
         
         </div>
         
