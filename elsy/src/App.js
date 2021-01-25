264952eba8716aae361be19952059e1c5e2576c3
Saurav Barua <myallmailofficial@gmail.com>
2021-01-25 15:14:42 +0100

    Add var constantes

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import './App.css';
 
+var constantes = [
+  {tempMin:-20},
+  {tempMax:40},
+  {heartMin:80},
+  {heartMax:180},
+  {stepsMin:0},
+  {stepsMax:50000}
+]
+
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="text-primary">Hello !</h1>
+        <h1 className="text-primary ">Hello !</h1>
       </div>
     );
   }
