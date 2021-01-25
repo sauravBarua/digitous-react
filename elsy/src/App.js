8a6cd3c979c39f9e31e8e2f4d8dc336af6170610
Saurav Barua <myallmailofficial@gmail.com>
2021-01-25 16:10:20 +0100

    Working on 02 - Affichage

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 
+
 var constantes = [
   {tempMin:-20},
   {tempMax:40},
@@ -8,13 +9,23 @@ var constantes = [
   {heartMax:180},
   {stepsMin:0},
   {stepsMax:50000}
-]
+];
 
 export class App extends React.Component {
   render() {
     return (
       <div>
         <h1 className="text-primary ">Hello !</h1>
+        <div className="container-fluid">
+        <div className="row">
+          <P>Heart :{this.constantes[2]} </P>
+          <P>Temperature :{this.constantes[0]}</P>
+          <P>Steps :{this.constantes[4]}</P>
+        
+        </div>
+        
+      </div>
+        
       </div>
     );
   }
