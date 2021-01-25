a5589c0b899fec8c3963b1dd343f9d9ea6a53567
Saurav Barua <myallmailofficial@gmail.com>
2021-01-25 20:31:53 +0100

    Display success

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,37 +1,28 @@
 import React from "react";
-import './App.css';
+ 
+class Home extends React.Component {
+	constantes ={
+    tempMin:-20,
+    tempMax: 40,
+    heartMin:80,
+    heartMax:180,
+    stepsMin:0,
+    stepsMax:50000
+  }
 
-
-var constantes = {
-"tempMin":-20,
-"tempMax":40,
-"heartMin": 80,
-"heartMax":180,
-"stepsMin":0,
-"stepsMax":50000
-
-
-};
-  
-
-export class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1 className="text-primary ">Hello !</h1>
-        <div className="container-fluid">
+			<div className="container-fluid">
         <div className="row">
-          <P>Heart :{constantes.heartMin} </P>
-          <P>Temperature :{constantes.tempMin}</P>
-          <P>Steps :{constantes.stepsMin}</P>
-        
+          <p>Heart :{this.constantes.heartMin} </p>
+          <p>Temperature : {this.constantes.tempMin} </p>
+          <p>Steps : {this.constantes.stepsMin} </p>
         </div>
-        
-      </div>
-        
-      </div>
-    );
-  }
+			
+
+			</div>
+		)
+  };
 }
 
-export default App;
+export default Home;
