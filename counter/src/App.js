c17dc73f9ac60f4a36c944240f47a0d95b300713
Saurav Barua <myallmailofficial@gmail.com>
2021-01-27 13:50:08 +0100

    Update App-js with increment and decrement

--- counter/src/App.js
+++ counter/src/App.js
@@ -8,10 +8,22 @@ class App extends React.Component {
     };
   }
 
+  decrementCount=()=>{
+    this.setState({count: this.state.count - 1})
+  }
+
+  incrementCount=()=>{
+    this.setState({count: this.state.count + 1})
+  }
+
   render() {
     return (
   <div>
     <h1>Counter</h1>
+    <h2> {this.state.count} </h2>
+    <button style={{backgroundColor: "red"}}  onClick={this.decrementCount}>-</button>
+
+    <button style={{backgroundColor: "green"}} onClick={this.incrementCount}>+</button>
   </div>
 )
   }
