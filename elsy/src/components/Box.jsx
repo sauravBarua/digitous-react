3b1853107548aabca94fd29150252b160f624c88
Saurav Barua <myallmailofficial@gmail.com>
2021-01-27 20:35:10 +0100

    Try to @ App-js with object and @Box-jsx with ES6 style

--- elsy/src/components/Box.jsx
+++ elsy/src/components/Box.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 class Box extends React.Component {
     render() {
+      const {color, value} = this.props;
         return (
             <div className="box col-sm-3 col-6">
-                <span className="material-icons" style={{color: this.props.color, fontSize: 100}}>
+                <span className="material-icons" style={{color: color, fontSize: 100}}>
                     {this.props.icon}
                 </span>
-                <p>{this.props.value} {this.props.unit}</p>
+                <p>{value} {this.props.unit}</p>
             </div>
         )
     }
