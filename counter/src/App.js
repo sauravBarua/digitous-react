5f2b5b9585c962149e6269cab2d548c8b4571aea
Saurav Barua <myallmailofficial@gmail.com>
2021-01-27 16:26:34 +0100

    Update Counter-jsx

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,29 +1,14 @@
 import React from "react";
+import Counter from "./components/Counter";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-    count: 0,
-    };
-  }
-
-  decrementCount=()=>{
-    this.setState({count: this.state.count - 1})
-  }
-
-  incrementCount=()=>{
-    this.setState({count: this.state.count + 1})
-  }
-
+  
+ 
   render() {
     return (
   <div>
-    <h1>Counter</h1>
-    <h2> {this.state.count} </h2>
-    <button style={{backgroundColor: "red"}}  onClick={this.decrementCount}>-</button>
-
-    <button style={{backgroundColor: "green"}} onClick={this.incrementCount}>+</button>
+  <Counter/>
+    
   </div>
 )
   }
--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 
 class Counter extends React.Component {
-  
-  
+  constructor(props) {
+    super(props);
+    this.state = { 
+    count: 0,
+    };
+  }
+
+  decrementCount=()=>{
+    this.setState({count: this.state.count - 1})
+  }
+
+  incrementCount=()=>{
+    this.setState({count: this.state.count + 1})
+  }
 
   
-
   render() {
     return (
   <div>
+    <h1>Counter</h1>
+    <h2> {this.state.count} </h2>
+    <button style={{backgroundColor: "red"}}  onClick={this.decrementCount}>-</button>
+
+    <button style={{backgroundColor: "green"}} onClick={this.incrementCount}>+</button>
     
   </div>
 )
