b1408c3948dd92651ceab19c8797b3cdc72e732c
Saurav Barua <myallmailofficial@gmail.com>
2021-02-01 18:27:49 +0100

    Update

--- form/src/App.js
+++ form/src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import From from './components/From';
-
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-      this.State = {
-        value: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
     }
   }
-  render() {
-    return (
-      <div>
-        <From />
-      </div>
-    )
+
+
+  onEmailChange = (event)=> {
+    this.setState({email: event.target.value})
   }
+
+
+  onPasswordChange=(event)=> {
+    this.setState({password: event.target.value})
+  }
+
+  handleSubmit =(event)=> {
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <form id="contact-form" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Email address</label>
+            <input type="email" className="form-control" value={this.state.email} onChange={this.onEmailChange} />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" value={this.state.password} onChange={this.onPasswordChange} />
+          </div>
+        
+          
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+
 }
+
 export default App;
