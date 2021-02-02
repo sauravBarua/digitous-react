a49a271e9957a4c82d1376df2f0533df3f79c1ad
Saurav Barua <myallmailofficial@gmail.com>
2021-02-02 14:19:50 +0100

    Create Add, List, Pay and Button jsx files

--- bakery/src/App.js
+++ bakery/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Add from './composants/Add';
+import List from './composants/List';
+import Pay from './composants/Pay';
+import Button from './composants/Button';
 
-export default class App extends Component {
+
+export default class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      activeTab: '',
+      items: []
+    }
+  }
+  
   render() {
     return (
       <div>
-        
+        <button/>
+        <button/>
+        <button/>
       </div>
     )
   }
---
+++ bakery/src/composants/Pay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Pay extends Recat.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
