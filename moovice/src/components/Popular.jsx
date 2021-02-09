c6d697d97966ec7e3d7f6d908ddbaea4d64b70be
sauravBarua <myallmailofficial@gmail.com>
2021-02-09 18:26:16 +0100

    @React promise je t'aime je t'aimais et je t'aimerais

--- moovice/src/components/Popular.jsx
+++ moovice/src/components/Popular.jsx
@@ -1,31 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Popular extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            movies: []
-        };
-      }
-      componentDidMount() {
-        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710")
-        .then((response) => {return response.json();})
-        .then((response) => {
-            this.setState({
-                movies: response.name
-            });
-        })
-        .catch((response) => {
-            this.setState({
-                movies: "Erreur lors de la recuperation"
-            });
-        })
-	}
-    render() {
-        return (
-            <div>
-              <h1>Popular</h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        this.setState({
+          movies: response,
+        });
+      })
+      .catch((response) => {
+        this.setState({
+          movies: "Erreur lors de la recuperation",
+        });
+      });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.movies.map((movie) => {
+          return {...movie}
+        })}
+      </div>
+    );
+  }
 }
