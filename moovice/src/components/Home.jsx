9d2e7a6c0be89ad6eda66e7ae23e381f65f6c910
sauravBarua <myallmailofficial@gmail.com>
2021-02-10 16:05:54 +0100

    Update and Working on it

--- moovice/src/components/Home.jsx
+++ moovice/src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Home extends Component {
+ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
+                Home
             </div>
         )
     }
 }
+export default Home;
--- moovice/src/components/Popular.jsx
+++ moovice/src/components/Popular.jsx
@@ -1,38 +1,51 @@
-import React, { Component } from "react";
+import React from 'react';
 
- class Popular extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: [],
-    };
-  }
+class Popular extends React.Component {
 
-  componentDidMount() {
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710"
-    )
-      .then((res) => res.json())
-      .then((moviesData) => {
-        this.setState({
-          movies: moviesData,
-        });
-      });
-  }
+    constructor(props) {
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.movies.map((movie)=>{
-            <li>
-              {movie.title}
-            </li>
-          })}
-        </ul>
-      </div>
-    )
-}}
+        super(props);
+
+        this.state = {
+            movies: [],
+        }
+
+    }
+
+  
+    componentDidMount() {
+
+        fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    movies: json,
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+  
+    render() {
+
+
+
+        return (
+            <div className="App">
+                <ul>
+                    {this.state.movies.map(movie => (
+                        <li>
+                            Title: {movie.title}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+
+    }
+
+}
 
 export default Popular;
-
