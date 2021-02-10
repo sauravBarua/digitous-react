b27631a8cebbc17bc6b8d808aa9766be82b54932
sauravBarua <myallmailofficial@gmail.com>
2021-02-10 18:45:23 +0100

    Update and I am trying to data fetch

--- moovice/src/components/Popular.jsx
+++ moovice/src/components/Popular.jsx
@@ -1,51 +1,38 @@
-import React from 'react';
+import React from "react";
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
+    this.state = {
+      movies: [],
+    };
+  }
 
-        super(props);
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          movies: [...data.results],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-        this.state = {
-            movies: [],
-        }
+  render() {
 
-    }
-
-  
-    componentDidMount() {
-
-        fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    movies: json,
-                })
-            }).catch((err) => {
-                console.log(err);
-            });
-
-    }
-
-  
-    render() {
-
-
-
-        return (
-            <div className="App">
-                <ul>
-                    {this.state.movies.map(movie => (
-                        <li>
-                            Title: {movie.title}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-
-    }
-
+    return (
+      <div>
+        Popular
+      </div>
+    );
+  }
 }
 
 export default Popular;
