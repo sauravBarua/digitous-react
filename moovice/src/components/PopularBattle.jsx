682e907f757313c68f3a82fa7ba66cd41dc0c504
sauravBarua <myallmailofficial@gmail.com>
2021-02-11 13:53:16 +0100

    Working on Popular Battle

--- moovice/src/components/PopularBattle.jsx
+++ moovice/src/components/PopularBattle.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
+import React from "react";
 
- class PopularBattle extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          movies: [],
-        };
-      }
-    
-      componentDidMount() {
-        fetch(
-          "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710"
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            console.log("I am from json", data);
-            this.setState({
-              movies: [...data.results],
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    render() {
-        return (
-            <div>
-                <h1>PopularBattle</h1>
-            </div>
-        )
-    }
+class PopularBattle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: [],
+      currentBattle: 0,
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba7a0b430074bec33494026c1ada710"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("I am from json", data);
+        this.setState({
+          movies: [...data.results],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>PopularBattle</h1>
+        
+      </div>
+    );
+  }
 }
-export default  PopularBattle;
+export default PopularBattle;
